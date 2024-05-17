667e216f895ea7c196819b55560b73276d497cf1
AntCannon <anthonycannonier@gmail.com>
2024-05-17 11:45:59 -0400

    test user input draw number of cards

--- src/components/Sandbox.jsx
+++ src/components/Sandbox.jsx
@@ -1,20 +1,49 @@
 import './Sandbox.css'
 import { useState, useStack } from 'react'
+import { useForm } from 'react-hook-form'
 import { shuffleDeck, drawFromDeck } from '../utils/deckFetch.js'
 
 export default function Sandbox(){
-  const [ deck, setDeck ] = useState([]);
-  const [ cardsDrawnFromDeck, setCardsDrawnFromDeck] = useState([])
+  const [ deck, setDeck ] = useState({})
+  const [ cardsDrawnFromDeck, setCardsDrawnFromDeck] = useState({
+    remaining: "",
+    cards: []
+  })
+
+  const cardDrawForm = useForm({
+    defaultValues: {
+      numCardsToDraw: 1,
+    }
+  })
+
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState,
+    reset
+  } = cardDrawForm;
 
   function handleShuffleDeck() {
     shuffleDeck()
   }
 
+  function onSubmit(input) {
+    const num = input.numCardsToDraw
+
+    drawFromDeck(num)
+    .then(deck => {
+      console.log(deck)
+      setCardsDrawnFromDeck(deck)
+    })
+    .catch(err => console.error(err))
+  }
+
   function handleDrawFromDeck() {
     drawFromDeck()
       .then(deck => {
-        console.log(deck.cards)
-        setCardsDrawnFromDeck(deck.cards)
+        console.log(deck)
+        setCardsDrawnFromDeck(deck)
       })
       .catch(err => console.error(err))
   }
@@ -23,17 +52,26 @@ export default function Sandbox(){
     <>
       Sandbox
       <button onClick={handleShuffleDeck}>Shuffle Deck</button>
-      <div className="card-draw-form">
-        
-        <button onClick={handleDrawFromDeck}>Draw a Card from Deck</button>
-      </div>
+      <form className="card-draw-form" onSubmit={handleSubmit(onSubmit)}>
+        <label>Number of Cards to Draw:
+          <br />
+          <input
+            className="input-deck-draw-count"
+            type="number"
+            id="numCardsToDraw"
+            {...register("numCardsToDraw")}
+          />
+        </label>
+        <button type="submit">Draw Card from Deck</button>
+      </form>
       <div>
         Card(s) Draw From Deck: 
         <div className="cards">
-          {cardsDrawnFromDeck.map(({code, image}) => (
+          {cardsDrawnFromDeck.cards.map(({code, image}) => (
             <img className="card" src={image} key={code} />
           ))}
         </div>
+        Cards Remaining in Deck: {cardsDrawnFromDeck.remaining}
       </div>
   
       
