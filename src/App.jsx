6dbcfb2d5e1c049b9d243dcc6d5896f30cd9e61a
AntCannon <anthonycannonier@gmail.com>
2024-05-18 16:07:28 -0400

    add window grid

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Sandbox from './components/Sandbox.jsx'
+import Play from './components/Play.jsx'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       </header>
       <main>
         Main
+        <Play />
         <Sandbox />
         {/* <Routes>
         </Routes> */}
---
+++ src/components/Play.jsx
@@ -0,0 +1,26 @@
+import './Play.css'
+
+export default function Play() {
+
+  return (
+    <>
+    <h2>Play!</h2>
+    <div className="container">
+      <div className="m"></div>
+      <div className="eR"></div>
+      <div className="eD"></div>
+      <div className="eP"></div>
+      <div className="psW"></div>
+      <div className="eB"></div>
+      <div className="pD"></div>
+      <div className="pP"></div>
+      <div className="pB"></div>
+      <div className="pW"></div>
+      <div className="pR"></div>
+      <div className="pA"></div>
+      <div className="i"></div>
+      <div className="a"></div>
+    </div>
+    </>
+  )
+}
