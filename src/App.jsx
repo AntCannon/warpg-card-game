9e372ec7069338ba5f10e56ad4cbe30dbcdc711d
AntCannon <anthonycannonier@gmail.com>
2024-05-26 14:08:55 -0400

    add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header.jsx'
+import Hero from './Hero.jsx' 
 import Field from './components/Field.jsx'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header />
       <main>
+        <Hero />
         <Field />
         {/* <Routes>
         </Routes> */}
---
+++ src/Hero.jsx
@@ -0,0 +1,16 @@
+import './Hero.css'
+
+export default function Hero() {
+
+  return (
+    <section className="hero">
+      <div className="hero-text">
+        <p className="top">waRPG</p>
+        <p className="main">War the card game REIMAGINED!</p>
+        <button className="bottom">Play Now!</button>
+      </div>
+      <div className="hero-img">
+      </div>
+    </section>
+  )
+}
